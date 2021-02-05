a175da428d47dd09a267a6df7bce35e03ac7fb98
shandilya01 <tushar01shandilya@gmail.com>
2021-02-06 00:47:33 +0530

    changed debug to True

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8eu0bk#_r^bn=xcj_q03((2!a7a8d0t-o=vflw@f0_9@wnh7^)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['portfolio-tusharshandilya.herokuapp.com' , '127.0.0.1']
 
